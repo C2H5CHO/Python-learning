cfbff603281773514790997d30c728902aa39303
C2H5CHO <2403692189@qq.com>
2025-07-19 01:55:49 +0800

    add 03_string_create --code

---
+++ 【Python 第一弹】/EP01：【Python 第一弹】基础入门知识/基础入门知识/02_logic.py
@@ -0,0 +1,15 @@
+"""
+逻辑运算
+"""
+
+# 1. and运算
+print(True and True)
+print(True and False)
+
+# 2. or运算
+print(True or False)
+print(False or False)
+
+# 3. not运算
+print(not True)
+print(not False)
---
+++ 【Python 第一弹】/EP01：【Python 第一弹】基础入门知识/基础入门知识/03_string_create.py
@@ -0,0 +1,18 @@
+"""
+创建字符串
+"""
+
+# 1. 单引号
+name = '李白'
+print(f'姓名：{name}')
+
+# 2. 双引号
+age = "18"
+print(f"年龄：{age}")
+
+# 3. 三引号
+hobby = """
+1. movie
+2. music
+"""
+print(f'兴趣：{hobby}')
