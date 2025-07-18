dd421cf83e68e676a2b79262a502e23ea12adb89
C2H5CHO <2403692189@qq.com>
2025-07-19 02:08:00 +0800

    Create 04_escape_character.py

---
+++ 【Python 第一弹】/EP01：【Python 第一弹】基础入门知识/基础入门知识/04_escape_character.py
@@ -0,0 +1,15 @@
+"""
+插入转义符
+"""
+
+# 1. \'
+string_1 = '老师：\'论文写完了嘛？\''
+print(string_1)
+
+# 2. \"
+string_2 = "学生：\"快了，快了。\""
+print(string_2)
+
+# 3. \n + \t
+string_3 = "老师：\n\t\"速度！速度！\""
+print(string_3)
