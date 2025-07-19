0aba8444fa4302650c7b247040a60e8e882a08b3
C2H5CHO <2403692189@qq.com>
2025-07-20 02:23:58 +0800

    Create 02_list.py

---
+++ 【Python 第一弹】/EP02：【Python 第一弹】复杂数据类型/复杂数据类型/02_list.py
@@ -0,0 +1,15 @@
+"""
+创建列表
+"""
+
+# 1. []创建
+lst1 = [1, 2, 3, 4, 5]
+lst2 = [1, 'two', 3.0, [4, 5]]
+print(lst1)
+print(lst2)
+
+# 2. list()创建
+lst3 = list(range(5))
+lst4 = list('Hello')
+print(lst3)
+print(lst4)
